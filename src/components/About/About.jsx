57f5de3519f6aeedabd1420e0ee07aaf0bfb920e
konstantin1111111111 <kryl0vconstantin@ya.ru>
2025-07-17 20:21:51 +0300

    обновляю

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,9 +6,7 @@ export function About() {
     <section className="section about">
       <h2 className="section__title">О проекте</h2>
       <p className="section__text">
-        Данный проект демонстрирует сконструированную мной модель блочного ТЭНа.
-
-   
+ Демонстрирую разработанную мной модель блочного ТЭНа.
       </p>
          <p className="section__title__model">
            Модель блочного ТЭНа
